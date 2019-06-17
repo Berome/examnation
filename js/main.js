e8fef8cf0d6783414f8abba619a720e364b28c98
Bartholomew <897843262@qq.com>
2019-06-17 22:02:43 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,52 +1,87 @@
-$(document).ready(function() {
+jQuery(document).ready(function($) {
 
-  $('a.blog-button').click(function() {
-    // If already in blog, return early without animate overlay panel again.
-    if (location.hash && location.hash == "#blog") return;
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
-    $('.main-post-list').removeClass('hidden');
-    currentWidth = $('.panel-cover').width();
-    if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed');
-    } else {
-      $('.panel-cover').css('max-width',currentWidth);
-      $('.panel-cover').animate({'max-width': '700px', 'width': '30%'}, 400, swing = 'swing', function() {} );
-    }
-  });
+	'use strict';
 
-  if (window.location.hash && window.location.hash == "#blog") {
-    $('.panel-cover').addClass('panel-cover--collapsed');
-    $('.main-post-list').removeClass('hidden');
-  }
+        $(window).load(function() { // makes sure the whole site is loaded
+            $(".seq-preloader").fadeOut(); // will first fade out the loading animation
+            $(".sequence").delay(500).fadeOut("slow"); // will fade out the white DIV that covers the website.
+        })
+      
+        
+        $(function() {
+  
+        function showSlide(n) {
+            // n is relative position from current slide
+          
+            // unbind event listener to prevent retriggering
+            $body.unbind("mousewheel");
+          
+            // increment slide number by n and keep within boundaries
+            currSlide = Math.min(Math.max(0, currSlide + n), $slide.length-1);
+            
+            var displacment = window.innerWidth*currSlide;
+            // translate slides div across to appropriate slide
+            $slides.css('transform', 'translateX(-' + displacment + 'px)');
+            // delay before rebinding event to prevent retriggering
+            setTimeout(bind, 700);
+            
+            // change active class on link
+            $('nav a.active').removeClass('active');
+            $($('a')[currSlide]).addClass('active');
+            
+        }
+      
+        function bind() {
+             $body.bind('false', mouseEvent);
+          }
+      
+        function mouseEvent(e, delta) {
+            // On down scroll, show next slide otherwise show prev slide
+            showSlide(delta >= 0 ? -1 : 1);
+            e.preventDefault();
+        }
+        
+        $('nav a, .main-btn a').click(function(e) {
+            // When link clicked, find slide it points to
+            var newslide = parseInt($(this).attr('href')[1]);
+            // find how far it is from current slide
+            var diff = newslide - currSlide - 1;
+            showSlide(diff); // show that slide
+            e.preventDefault();
+        });
+      
+        $(window).resize(function(){
+          // Keep current slide to left of window on resize
+          var displacment = window.innerWidth*currSlide;
+          $slides.css('transform', 'translateX(-'+displacment+'px)');
+        });
+        
+        // cache
+        var $body = $('body');
+        var currSlide = 0;
+        var $slides = $('.slides');
+        var $slide = $('.slide');
+      
+        // give active class to first link
+        $($('nav a')[0]).addClass('active');
+        
+        // add event listener for mousescroll
+        $body.bind('false', mouseEvent);
+    })        
 
-  if (window.location.pathname.substring(0, 5) == "/tag/") {
-    $('.panel-cover').addClass('panel-cover--collapsed');
-  }
 
-  $('.btn-mobile-menu__icon').click(function() {
-    if ($('.navigation-wrapper').css('display') == "block") {
-      $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-        $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
-        $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
-      });
-      $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
+        $('#form-submit .date').datepicker({
+        });
 
-    } else {
-      $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
-    }
-    $('.btn-mobile-menu__icon').toggleClass('fa fa-bars fa fa-chevron-circle-up animated fadeIn');
-  });
 
-  $('.navigation-wrapper .blog-button').click(function() {
-    if ($('.navigation-wrapper').css('display') == "block") {
-      $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-        $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
-        $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
-      });
+        $(window).on("scroll", function() {
+            if($(window).scrollTop() > 100) {
+                $(".header").addClass("active");
+            } else {
+                //remove the background property so it comes transparent again (defined in your css)
+               $(".header").removeClass("active");
+            }
+        });
 
-      $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
-    }
-    
-    $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
-  });
+
 });
